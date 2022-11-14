ffb6d7f3419882e9ccf0f51ee3a432b4b1f724d3
david-bu <davidbusse343@gmx.de>
2022-11-14 13:48:44 +0100

    commented

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -28,6 +28,14 @@ public class Sudoku {
         System.out.println(isValidSudokuSolution(validSudoku));
     }
 
+    /**
+     * tests if the array has the right layout format and:
+     *  - in every column there are all numbers between 1 and 9 once
+     *  - in every row there are all numbers between 1 and 9 once
+     *  - in every 3x3 matrix there are all numbers between 1 and 9 once
+     * @param sudoku the sudoku to validate
+     * @return true if it's a valid solution
+     */
     public static boolean isValidSudokuSolution(int[][] sudoku) {
         if (!isValidLayout(sudoku)) {
             System.out.println("Wrong sudoku layout!");
@@ -36,6 +44,11 @@ public class Sudoku {
         return areColumnsValid(sudoku) && areRowsValid(sudoku) && areMatricesValid(sudoku);
     }
 
+    /**
+     * validates that in every column there's every number between 1 and 9 once
+     * @param sudoku the sudoku where the columns should be validated
+     * @return true if all columns are valid
+     */
     private static boolean areColumnsValid(int[][] sudoku) {
         boolean[] values = new boolean[9];
         for (int row = 0; row < 9; row++) {
@@ -52,6 +65,11 @@ public class Sudoku {
         return true;
     }
 
+    /**
+     * validates that in every row there's every number between 1 and 9 once
+     * @param sudoku the sudoku where the rows should be validated
+     * @return true if all rows are valid
+     */
     private static boolean areRowsValid(int[][] sudoku) {
         boolean[] values = new boolean[9];
         for (int column = 0; column < 9; column++) {
@@ -68,6 +86,11 @@ public class Sudoku {
         return true;
     }
 
+    /**
+     * validates that in every 3x3 matrix there's every number between 1 and 9 once
+     * @param sudoku the sudoku where the 3x3 matrices should be validated
+     * @return true if all 3x3 matrices are valid
+     */
     private static boolean areMatricesValid(int[][] sudoku) {
         for (int matrixRow = 0; matrixRow < 3; matrixRow++) {
             for (int matrixColumn = 0; matrixColumn < 3; matrixColumn++) {
@@ -78,6 +101,14 @@ public class Sudoku {
         return true;
     }
 
+    /**
+     * the values in sudoku[top][left] to sudoku[top+2][left+2] will be validated (every num between 1 and 9 once)
+     * indices are NOT validated
+     * @param sudoku the sudoku where a single 3x3 matrix should be validated
+     * @param top the index of the first row
+     * @param left the index of the first column
+     * @return true if this single 3x3 matrix is valid
+     */
     private static boolean isMatrixValid(int[][] sudoku, int top, int left) {
         boolean[] values = new boolean[9];
         setBooleanArrayFalse(values);
@@ -93,12 +124,21 @@ public class Sudoku {
         return isBooleanArrayTrue(values);
     }
 
+    /**
+     * sets every value in values to false
+     * @param values the array to reset to false
+     */
     private static void setBooleanArrayFalse(boolean[] values) {
         // or Arrays.fill(values, false);
         for (int i = 0; i < values.length; i++)
             values[i] = false;
     }
 
+    /**
+     * checks if every value in values is true
+     * @param values the array to check
+     * @return true if all elements in values are true
+     */
     private static boolean isBooleanArrayTrue(boolean[] values) {
         for (boolean value : values) {
             if (!value)
@@ -107,6 +147,11 @@ public class Sudoku {
         return true;
     }
 
+    /**
+     * checks if the sudoku array is int[9][9]
+     * @param sudoku the array to validate
+     * @return true if valid
+     */
     private static boolean isValidLayout(int[][] sudoku) {
         return (sudoku != null && sudoku.length == 9 &&
                 sudoku[0] != null && sudoku[0].length == 9 &&
@@ -114,4 +159,5 @@ public class Sudoku {
                 sudoku[2] != null && sudoku[2].length == 9
         );
     }
+
 }
